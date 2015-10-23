9e2807bf226925d6a03c5c3c90e1d8318fca7353
Jered Spears <jeredspears.c@gmail.com>
2015-10-23 00:56:17 -0500

    debugging heroku

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 var express   = require('express'),
 		path      = require('path');
 		routes    = require('./src/routes/routes.js'),
-		favicon   = require('static-favicon');
 var	app = express();
 
 
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'src\\views'));
 
-app.use(favicon());
 app.use(routes.router);
 app.use('/static', express.static(path.join(__dirname,'public')));
 
